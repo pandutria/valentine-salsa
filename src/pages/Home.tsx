3e130c0fd143c3aceb41cfdad8e4843ad831dd74
Pandu Tria Adyatama <panduadyatama029@gmail.com>
2026-02-10 22:14:08 +0700

    first commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   const [showHint, setShowHint] = useState<boolean>(false)
   const navigate = useNavigate()
   
-  const SECRET_CODE = "17" // Ganti dengan code yang kamu mau
+  const SECRET_CODE = "199" // Ganti dengan code yang kamu mau
   
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
